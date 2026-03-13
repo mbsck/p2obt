c8277327e105256f7a3d4cfd6756f94c5d091b59
mbsck <code@mbscheuck.com>
2026-03-13 18:34:07 +0100

    fix(compose): array input now allows for strings or list of strings

--- examples/create_ob.py
+++ examples/create_ob.py
@@ -8,40 +8,36 @@ either create it locally as an (.obx)-file or directly upload
 it to ESO's P2-environment.
 """
 
-# %%
 from pathlib import Path
 
 from p2obt import OPTIONS, create_ob
 
-# %%
-# Specify and create the path for the 'manualOBs' directory.
+# NOTE: Specify and create the path for the 'manualOBs' directory.
 output_dir = Path("../assets") / "obs"
 output_dir.mkdir(parents=True, exist_ok=True)
 
-# %%
-# Locally creating an (.obx)-file
+# NOTE: Locally creating an (.obx)-file
 # ------------------------
-#
-# Create a science target (.ob)
+
+# NOTE: Create a science target (.ob)
 # Create an (.obx)-file for a science target for 'GRA4MAT'. If the
 # 'ouput_dir' keyword is passed, an (.obx)-file gets created.
-create_ob("HD 142666", "sci", "uts", mode="gr", output_dir=output_dir)
+create_ob("HD 142666", "sci", "UTs", mode="gr", output_dir=output_dir)
 
-# %%
-# Create an (.obx)-file for a calibrator for the UT-array configuration
+
+# NOTE: Create an (.obx)-file for a calibrator for the UT-array configuration
 # for the science target 'HD 142666' and for 'GRA4MAT', tagged as an "L"
 # band calibrator.
 create_ob(
     "HD 100920",
     "cal",
-    "uts",
+    "UTs",
     sci_name="HD 142666",
     tag="L",
     mode="gr",
     output_dir=output_dir,
 )
 
-# %%
 # NOTE: Change constraint settings
 # Turbulence can be choosen from 10%, 30% or 70%. Default is 30%.
 OPTIONS.constraints.turbulence = 70
@@ -50,11 +46,10 @@ OPTIONS.constraints.turbulence = 70
 # 'clear', 'thin' and 'variable'. Default is 'clear'.
 OPTIONS.constraints.transparency = "thin"
 
-# %%
-# Direct upload of an ob
+# NOTE: Direct upload of an OB
 # ------------------------
-#
-# If the 'container_id' keyword is passed then the (.obx)-file will be
+
+# NOTE: If the 'container_id' keyword is passed then the (.obx)-file will be
 # uploaded and if the 'connection'-keyword is none it will ask for your login
 # data.
 # For this example the ob will be uploaded to ESO's demo environment
@@ -64,7 +59,7 @@ OPTIONS.constraints.transparency = "thin"
 create_ob(
     "HD 100920",
     "cal",
-    "uts",
+    "UTs",
     sci_name="HD 142666",
     mode="gr",
     container_id=3001786,
